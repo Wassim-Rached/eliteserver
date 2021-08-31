3507295f1c809dbad193de0bb4ffdd343820a869
Wassim-Rached <wassimrached404@gmail.com>
2021-08-31 12:32:01 +0100

    hi

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.use('/api/News',NewsRouter)
 app.use('/api/users',UserRouter)
 app.use('/api/rules',RulesRouter)
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static('client/build'))
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
