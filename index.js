ac91f17a61a25b115fea87b0b0d1ae50adb0d208
Wassim-Rached <wassimrached404@gmail.com>
2021-09-01 12:50:27 +0100

    hh

--- index.js
+++ index.js
@@ -30,11 +30,13 @@ app.use('/api/News',NewsRouter)
 app.use('/api/users',UserRouter)
 app.use('/api/rules',RulesRouter)
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  })
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.listen(PORT,()=>{
